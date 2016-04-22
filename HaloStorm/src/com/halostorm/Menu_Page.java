2ffd2108700a03e5b01af1266101b410b7efc6f6
sedoaba <lebza.sedoaba@gmail.com>
2016-04-22 10:58:49 +0200

    Tracks and Music Player Button
    Added a new folder name Raw which holds 10 tracks, and the music player
    button works

--- HaloStorm/src/com/halostorm/Menu_Page.java
+++ HaloStorm/src/com/halostorm/Menu_Page.java
@@ -2,15 +2,51 @@ package com.halostorm;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu_Page extends Activity {
+	
+	public Button Btn_Playlist,Btn_Login;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_menu);
+		
+		Btn_Playlist = (Button)findViewById(R.id.btnplayer);
+		Btn_Login = (Button)findViewById(R.id.btnlogin);
+		
+		
+		//Music Player event listener
+		Btn_Playlist.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				//starting a activity
+				Intent music = new Intent(getApplicationContext(), Playlist_Screen.class);
+				startActivity(music);
+				
+			}
+		});
+		
+		//login event listener
+		Btn_Login.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		
 	}
+	
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
---
+++ HaloStorm/src/com/halostorm/Playlist_Screen.java
@@ -0,0 +1,18 @@
+package com.halostorm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Playlist_Screen extends Activity {
+	
+	
+	//creating arraylists to hold music title,description,image path and mp3 path
+	
+	
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_playlist);
+	}
+
+}
